6d5755339bb90fa6f8d60ae1af1179b294b77b67
GodwinJoy <godwinjoy277@gmail.com>
2025-09-21 21:39:41 +0530

    frontend2

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,3 +1,3 @@
 import { io } from 'socket.io-client';
 
-export const socket = io('http://chatapp-server-86wn.onrender.com/'); // Node WebSocket server
+export const socket = io('https://chatapp-server-86wn.onrender.com'); // Node WebSocket server
